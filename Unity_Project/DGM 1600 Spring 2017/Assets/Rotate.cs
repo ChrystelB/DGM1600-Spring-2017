5ad9e5fad82580fb88d1d638f0eb99d90291be41
Chrystel <chrystelbardsley@gmail.com>
2017-01-26 20:02:34 -0700

    Updates on the mantis.
    Now "De Bug"

---
+++ Unity_Project/DGM 1600 Spring 2017/Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public int number = 10;
+	public int rotSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(number);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (0,2,0);
+		transform.Rotate(Vector3.up,rotSpeed*Time.deltaTime);
+	
+	}
+}
