bdd03ed16657c413f0fa1e1700ee3715ae6830ea
Chrystel <chrystelbardsley@gmail.com>
2017-02-02 18:41:29 -0700

    Conventions and Syntax
    Helpful notes on the begining vocab for coding.

---
+++ Classes/Conventions_and_Syntax.cs
@@ -0,0 +1,26 @@
+//Conventions and Syntax
+
+Syntax(es) - Affects how the code runs. How things are written. Identifies when code is incorrect.
+/*
+*/
+
+Conventions - How the code is written. Doesnt affect how program runs. Formatting including white space and indents.
+/*
+*Indenting - Asthetically helpful with your structure and organization.
+    Ex:     Yup.
+
+*The Dot operator - dots between words. The Dots sygnifies us going further into the first word.
+A city from a Country.  
+    Ex: Country.State.City or Transformation.Position
+
+*The SemiColon - Used at the end of a statement within a class or group;
+    Ex;
+
+*Braces - The opening and closing of a statement. Also Identifies when code blocks should start and end.   
+    Ex: {Voila}
+
+*Comments - Used to comment about a piece of code. Helps other viewers or helpers to identify sections of
+the code.
+    EX: //This should be helpful.
+
+*/ 
