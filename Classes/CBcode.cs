f9cce719b20d3f9e1e359842e00e29d445deb345
Chrystel <chrystelbardsley@gmail.com>
2017-02-02 18:17:01 -0700

    Intro to classes
    The three Necessary things to have on hand.

---
+++ Classes/CBcode.cs
@@ -0,0 +1,25 @@
+// Class Example
+
+public class classes: MonoBehaviour
+{
+    public class Swords
+    {
+        public int Iron = 4;
+        public int Kryptonite = 16;
+        public int steel = 8;
+        public int lead = 5;
+    }
+    public class Icecream
+    {
+        public int strawberry = 1;
+        public int chocolate = 1;
+        public int vanilla = 1;
+    }
+    public class Toys
+    {
+        public int teddybear = 2;
+        public int hoolahoop = 2;
+        public int bouncyball = 2;
+    }
+//Things to always have on hand.
+}
