2b1e6557b46a6faa63f37008d2fe635c11ff76cd
Chrystel <chrystelbardsley@gmail.com>
2017-03-09 19:54:24 -0700

    Menu

---
+++ Unity_Project/DGM 1600 Spring 2017/Assets/BeginLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginLevel : MonoBehaviour {
+
+        public void LoadLevel(string level){
+
+            SceneManager.LoadScene(level);
+
+/*        }
+        public staitc void (Array level){
+            
+
+*/
+        }
+	
+}
