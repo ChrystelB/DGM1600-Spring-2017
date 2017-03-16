9ac61c21f77c7846fd760c49976fd27081ed1fe7
Chrystel <chrystelbardsley@gmail.com>
2017-03-16 16:36:59 -0600

    Array for StartUp Button

--- Unity_Project/DGM 1600 Spring 2017/Assets/Arrays(Button).cs
+++ Unity_Project/DGM 1600 Spring 2017/Assets/Arrays(Button).cs
@@ -1,28 +1,20 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
-
-
-/* arrays are ways of storing the same type of information together.
-A good example from the video, is buying an egg carton. When looking at the eggs, 
-the eggs wouldn't be that different from the others. Basically the "egg carton" 
-would be filled with variables. So, you would write the Name of the Array [then an
-index value] = value of integer. Here is a few examples.
-
-(One way to write it out)
 
 
 public class Arrays : MonoBehaviour {
 	
-	int[] myIntArray = new int[5];
+	public string[] StartUp = [Trees, Bushes, Buildings];
 
 
 	void Start ()
 	{
-		myIntArray[0] = 12;
-		myIntArray[0] = 13;
-		myIntArray[0] = 14;
-		myIntArray[0] = 15;
+		StartUp[Trees] = 12;
+		StartUp[Bushes] = 30;
+		StartUp[Buildings] = 6;
 	}
+}
 
 /*
 An easier way of doing it would be to list the integers in the same parenthesis. 
@@ -48,7 +40,7 @@ public class Arrays : MonoBehaviour {
 
 public class Arrays : MonoBehaviour {
 
-    public string[] friends = {"Jayson", "Debug", "Bianca","Anthony","Chrystel};
+    public string[] friends = {"Jayson", "Debug", "Bianca","Anthony","Chrystel"};
 
 	void Start ()
 	{
@@ -59,12 +51,12 @@ public class Arrays : MonoBehaviour {
 
 	}
 }
-Which brings us to what a "list" is in coding/scripting language. A list is an object which holds
+
+ Which brings us to what a "list" is in coding/scripting language. A list is an object which holds
 variables in a specific order. Normally it is stated beforehand the extent to which your list will go to. 
 With that, there are many different things that you can do with your list. For instance, you can do the "get"
 action, or add, or remove numbers held within the list. A quick example of these type of actions will follow, 
 otherwise you can refer to the past arrays and lists. 
-
 
 List<string> Animals = new List<string>();
 {
