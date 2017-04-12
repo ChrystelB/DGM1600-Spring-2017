a74bdee96a6500d7a2a2352d68bdf23e6ea49ee0
Chrystel <chrystelbardsley@gmail.com>
2017-04-12 10:52:42 -0600

    Calculator

---
+++ Unity_Project/DGM 1600 Spring 2017/Assets/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour {
+
+int a = 0;
+int b = 0;
+int c = 0;
+
+public InputField A;
+public InputField B;
+public Text C;
+
+public Button calcButton;
+
+void Start(){
+        Button btn = calcButton.GetComponent<Button>();
+      // btn.onClick.AddListener(Addition);
+}
+
+void Addition()
+{
+    a = int.Parse(A.text);
+    b = int.Parse(B.text);
+    
+    C.text = (a + b).ToString();
+}
+}
