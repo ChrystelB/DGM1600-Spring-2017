ee2226ca64b2b1e259add2881f1af06daa869981
Chrystel <chrystelbardsley@gmail.com>
2017-04-27 20:48:55 -0600

    Calc Updates

---
+++ Unity_Project/DGM 1600 Spring 2017/Calculator/Assets/Numbers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Numbers : MonoBehaviour {
+
+	public string ButtonText;
+
+	public InputField Value1;
+
+	void start (){
+		ButtonText = GetComponentInChildren<Text>().text;	
+	}
+
+	public void OnButtonClick(){
+		Value1.text += ButtonText;
+	}
+
+}
---
+++ Unity_Project/DGM 1600 Spring 2017/Calculator/Assets/Switch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour {
+
+	public List<Numbers> numbers;
+
+	public void SwitchField(){
+		foreach (Numbers button in numbers) {
+			button.Value1 = GetComponent<InputField> ();
+
+		}
+	} 
+
+
+}
